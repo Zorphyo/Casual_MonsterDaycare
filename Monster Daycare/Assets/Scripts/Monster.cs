186be4e246e501e7ce5318a2b42d9394f19abfe1
Zorphyo <zorphyo@outlook.com>
2024-10-02 15:35:10 -0400

    Multiple Tasks Added and Test Builds

--- Monster Daycare/Assets/Scripts/Monster.cs
+++ Monster Daycare/Assets/Scripts/Monster.cs
@@ -18,10 +18,14 @@ public class Monster : MonoBehaviour
     [SerializeField] SkinnedMeshRenderer middleBaby;
     [SerializeField] SkinnedMeshRenderer finalBaby;
 
-    float timeForTask;
+    float timeForTask1, timeForTask2, timeForTask3;
     float holdingTimeForTask;
-    [HideInInspector] public bool isTaskActive = false;
+    [HideInInspector] public bool isTask1Active = false;
+    [HideInInspector] public bool isTask2Active = false;
+    [HideInInspector] public bool isTask3Active = false;
+
     public List<string> itemNames;
+    string task1Item, task2Item, task3Item;
 
     int random;
     int minTime, maxTime;
@@ -45,17 +49,19 @@ public class Monster : MonoBehaviour
         {
             minTime = 12;
             maxTime = 15;
-            timeForTask = 15.0f;
+            holdingTimeForTask = 40.0f;
         }
 
         else 
         {
             minTime = 7;
             maxTime = 10;
-            timeForTask = 10.0f;
+            holdingTimeForTask = 10.0f;
         }
 
-        holdingTimeForTask = timeForTask;
+        timeForTask1 = holdingTimeForTask;
+        timeForTask2 = holdingTimeForTask;
+        timeForTask3 = holdingTimeForTask;
 
         random = Random.Range(minTime, maxTime);
 
@@ -65,21 +71,21 @@ public class Monster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isTaskActive)
+        if (isTask1Active)
         {
             taskTimer1Text.enabled = true;
             taskTimer1Text.gameObject.SetActive(true);
 
-            task1Text.text = "1. Get " + itemNames[random - 1];
+            task1Text.text = "1. Get " + task1Item;
 
-            if (timeForTask > 1)
+            if (timeForTask1 > 1)
             {
-                timeForTask -= Time.deltaTime;
+                timeForTask1 -= Time.deltaTime;
             }
 
             else
             {
-                timeForTask = 0;
+                timeForTask1 = 0;
                 taskTimer1Text.enabled = false;
                 taskTimer1Text.gameObject.SetActive(false);
 
@@ -91,7 +97,6 @@ public class Monster : MonoBehaviour
                     heart3.gameObject.SetActive(false);
 
                     mesh.sharedMesh = middleBaby.sharedMesh;
-                    //int counter = 0;
 
                     Material[] newMaterials = new Material[4];
                     newMaterials[0] = middleBaby.sharedMaterials[0];
@@ -109,7 +114,6 @@ public class Monster : MonoBehaviour
                     heart2.gameObject.SetActive(false);
 
                     mesh.sharedMesh = finalBaby.sharedMesh;
-                    //int counter2 = 0;
                     
                     Material[] newMaterials = new Material[4];
                     newMaterials[0] = finalBaby.sharedMaterials[0];
@@ -129,11 +133,11 @@ public class Monster : MonoBehaviour
                 }
 
                 hearts--;
-                isTaskActive = false;
-                timeForTask = holdingTimeForTask;
+                isTask1Active = false;
+                timeForTask1 = holdingTimeForTask;
             }
 
-            int seconds = Mathf.FloorToInt(timeForTask % 60);
+            int seconds = Mathf.FloorToInt(timeForTask1 % 60);
             taskTimer1Text.text = string.Format("{0:0}", seconds); 
         }
 
@@ -145,35 +149,237 @@ public class Monster : MonoBehaviour
             task1Text.text = "1. ";
         }
 
+        if (isTask2Active)
+        {
+            taskTimer2Text.enabled = true;
+            taskTimer2Text.gameObject.SetActive(true);
+
+            task2Text.text = "2. Get " + task2Item;
+
+            if (timeForTask2 > 1)
+            {
+                timeForTask2 -= Time.deltaTime;
+            }
+
+            else
+            {
+                timeForTask2 = 0;
+                taskTimer2Text.enabled = false;
+                taskTimer2Text.gameObject.SetActive(false);
+
+                task2Text.text = "2. ";
+
+                if (hearts == 3)
+                {
+                    heart3.enabled = false;
+                    heart3.gameObject.SetActive(false);
+
+                    mesh.sharedMesh = middleBaby.sharedMesh;
+
+                    Material[] newMaterials = new Material[4];
+                    newMaterials[0] = middleBaby.sharedMaterials[0];
+                    newMaterials[1] = middleBaby.sharedMaterials[1];
+                    newMaterials[2] = middleBaby.sharedMaterials[2];
+                    newMaterials[3] = middleBaby.sharedMaterials[3];
+
+                    mesh.sharedMaterials = newMaterials;
+                    
+                }
+
+                else if (hearts == 2)
+                {
+                    heart2.enabled = false;
+                    heart2.gameObject.SetActive(false);
+
+                    mesh.sharedMesh = finalBaby.sharedMesh;
+                    
+                    Material[] newMaterials = new Material[4];
+                    newMaterials[0] = finalBaby.sharedMaterials[0];
+                    newMaterials[1] = finalBaby.sharedMaterials[1];
+                    newMaterials[2] = finalBaby.sharedMaterials[2];
+                    newMaterials[3] = finalBaby.sharedMaterials[3];
+
+                    mesh.sharedMaterials = newMaterials;
+                    
+                }
+
+                else 
+                {
+                    heart1.enabled = false;
+                    heart1.gameObject.SetActive(false);
+                    timer.gameOver = true;
+                }
+
+                hearts--;
+                isTask2Active = false;
+                timeForTask2 = holdingTimeForTask;
+            }
+
+            int seconds = Mathf.FloorToInt(timeForTask2 % 60);
+            taskTimer2Text.text = string.Format("{0:0}", seconds); 
+        }
+
+        else 
+        {
+            taskTimer2Text.enabled = false;
+            taskTimer2Text.gameObject.SetActive(false);
+
+            task2Text.text = "2. ";
+        }
+
+        if (isTask3Active)
+        {
+            taskTimer3Text.enabled = true;
+            taskTimer3Text.gameObject.SetActive(true);
+
+            task3Text.text = "3. Get " + task3Item;
+
+            if (timeForTask3 > 1)
+            {
+                timeForTask3 -= Time.deltaTime;
+            }
+
+            else
+            {
+                timeForTask3 = 0;
+                taskTimer3Text.enabled = false;
+                taskTimer3Text.gameObject.SetActive(false);
+
+                task3Text.text = "3. ";
+
+                if (hearts == 3)
+                {
+                    heart3.enabled = false;
+                    heart3.gameObject.SetActive(false);
+
+                    mesh.sharedMesh = middleBaby.sharedMesh;
+
+                    Material[] newMaterials = new Material[4];
+                    newMaterials[0] = middleBaby.sharedMaterials[0];
+                    newMaterials[1] = middleBaby.sharedMaterials[1];
+                    newMaterials[2] = middleBaby.sharedMaterials[2];
+                    newMaterials[3] = middleBaby.sharedMaterials[3];
+
+                    mesh.sharedMaterials = newMaterials;
+                    
+                }
+
+                else if (hearts == 2)
+                {
+                    heart2.enabled = false;
+                    heart2.gameObject.SetActive(false);
+
+                    mesh.sharedMesh = finalBaby.sharedMesh;
+                    
+                    Material[] newMaterials = new Material[4];
+                    newMaterials[0] = finalBaby.sharedMaterials[0];
+                    newMaterials[1] = finalBaby.sharedMaterials[1];
+                    newMaterials[2] = finalBaby.sharedMaterials[2];
+                    newMaterials[3] = finalBaby.sharedMaterials[3];
+
+                    mesh.sharedMaterials = newMaterials;
+                    
+                }
+
+                else 
+                {
+                    heart1.enabled = false;
+                    heart1.gameObject.SetActive(false);
+                    timer.gameOver = true;
+                }
+
+                hearts--;
+                isTask3Active = false;
+                timeForTask3 = holdingTimeForTask;
+            }
+
+            int seconds = Mathf.FloorToInt(timeForTask3 % 60);
+            taskTimer3Text.text = string.Format("{0:0}", seconds); 
+        }
+
+        else 
+        {
+            taskTimer3Text.enabled = false;
+            taskTimer3Text.gameObject.SetActive(false);
+
+            task3Text.text = "3. ";
+        }
+
         if (timer.gameOver)
         {
-            isTaskActive = false;
+            isTask1Active = false;
+            isTask2Active = false;
+            isTask3Active = false;
             CancelInvoke();
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(isTaskActive && player.hasItem == true && player.heldItem == itemNames[random - 1])
+        if(isTask1Active && player.hasItem == true && player.heldItem == task1Item)
         {
             player.GiveItem();
-            isTaskActive = false;
+            isTask1Active = false;
 
-            timeForTask = holdingTimeForTask;
+            timeForTask1 = holdingTimeForTask;
             taskTimer1Text.enabled = false;
             taskTimer1Text.gameObject.SetActive(false);
 
             task1Text.text = "1. ";
         }
+
+        if(isTask2Active && player.hasItem == true && player.heldItem == task2Item)
+        {
+            player.GiveItem();
+            isTask2Active = false;
+
+            timeForTask2 = holdingTimeForTask;
+            taskTimer2Text.enabled = false;
+            taskTimer2Text.gameObject.SetActive(false);
+
+            task2Text.text = "2. ";
+        }
+
+        if(isTask3Active && player.hasItem == true && player.heldItem == task3Item)
+        {
+            player.GiveItem();
+            isTask3Active = false;
+
+            timeForTask3 = holdingTimeForTask;
+            taskTimer3Text.enabled = false;
+            taskTimer3Text.gameObject.SetActive(false);
+
+            task1Text.text = "3. ";
+        }
     }
 
     void CreateTask()
     {
-        if (!isTaskActive)
+        if (!isTask1Active)
         {
-            isTaskActive = true;
+            isTask1Active = true;
 
             random = Random.Range(1, itemNames.Count);
+
+            task1Item = itemNames[random - 1];
+        }
+
+        else if (!isTask2Active)
+        {
+            isTask2Active = true;
+
+            random = Random.Range(1, itemNames.Count);
+
+            task2Item = itemNames[random - 1];
+        }
+
+        else if (!isTask3Active)
+        {
+            isTask3Active = true;
+
+            random = Random.Range(1, itemNames.Count);
+
+            task3Item = itemNames[random - 1];
         }
     }
 }
